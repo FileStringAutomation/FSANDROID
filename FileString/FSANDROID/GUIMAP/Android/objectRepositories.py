ce7a2bd0339463df41c806100fab42314fabd3d8
Linh Nguyen <linh.nguyend@filestring.com>
2015-03-13 20:11:17 +0700

    Start revoke access

--- FileString/FSANDROID/GUIMAP/Android/objectRepositories.py
+++ FileString/FSANDROID/GUIMAP/Android/objectRepositories.py
@@ -18,7 +18,10 @@ SignIn_SignInButton = "id=com.filestring.lattedouble:id/btnSignIn"
 SignIn_ForgotPassword = "id=com.filestring.lattedouble:id/forget_password"
 SignIn_SignUp = "id=com.filestring.lattedouble:id/orSignUp"
 
-
+SignIn_Popup_ResendEmailId = "id=com.filestring.lattedouble:id/dlg_btn_negative"
+SignIn_Popup_CloseId = "id=com.filestring.lattedouble:id/dlg_btn_positive"
+SignIn_Popup_ResendEmailText = "identifier=Resend Email"
+SignIn_Popup_CloseText = "identifier=Close"
 
 """
 @Author: Minh.nguyen
@@ -34,12 +37,6 @@ SignUp_ShowPassword = "id=com.filestring.lattedouble:id/ckbSignUp_ShowPass"
 SignUp_CreateButton = "id=com.filestring.lattedouble:id/txtSignUp_Create"
 SignUp_SignIn = "id=com.filestring.lattedouble:id/back_signin"
 
-
-OK_Button = "id=com.filestring.lattedouble:id/dlg_btn_positive"
-Resend_Email_ButtonById = "id=com.filestring.lattedouble:id/dlg_btn_negative"
-Close_ButtonById = "id=com.filestring.lattedouble:id/dlg_btn_positive"
-Resend_Email_ButtonByText = "identifier=Resend Email"
-Close_ButtonByText = "identifier=Close"
 """
 @Author: Linh.nguyen
 @Date Created: Jan 07, 2015
@@ -48,18 +45,21 @@ Close_ButtonByText = "identifier=Close"
 KeyEvent_Home = 3
 KeyEvent_Back = 4
 KeyEvent_Menu = 82
-
-
+Popup_OkButton = "id=com.filestring.lattedouble:id/dlg_btn_positive"
+Popup_CancelButton = "id=com.filestring.lattedouble:id/dlg_btn_negative"
+Popup_Title = "id=com.filestring.lattedouble:id/dlg_title"
 
 """
 @Author: Linh.nguyen
 @Date Created: Jan 13, 2015
 @Description: Verify Page Contain by Text
 """
-Verify_SignIn_Form = "Forgot your password?"
-Verify_SignUP_Form= "New Account"
-Verify_AllFiles_Screen = "All Files"
-Verify_UserInfo_Screen = "Sign Out from FileString"
+Verify_Screen_SignIn = "Forgot your password?"
+Verify_Screen_SignUp= "New Account"
+Verify_Screen_AllFiles = "All Files"
+Verify_Screen_UserInfo = "User Info"
+Verify_Popup_NoInternetAccessTitle = "No Internet access"
+Verify_Popup_ServiceErrorTitle = "Service is temporarily unavailable"
 
 """
 @Author: Minh.nguyen
@@ -90,21 +90,21 @@ Home_EmptyText = "id=com.filestring.lattedouble:id/empty_text"
 """
 NavigationBar_Avatar = "id=com.filestring.lattedouble:id/drawer_avatar"
 NavigationBar_Username = "id=com.filestring.lattedouble:id/drawer_name"
-NavigationBar_Email = "id=com.filestring.lattedouble:id/drawer_email"
-NavigationBar_Notification_Bell = "id=com.filestring.lattedouble:id/drawer_notifications"
-NavigationBar_User_Expand = "id=com.filestring.lattedouble:id/drawer_users_expand"
-NavigationBar_All_Files = "id=com.filestring.lattedouble:id/drawer_my_files"
-NavigationBar_New_Received_Files = "id=com.filestring.lattedouble:id/drawer_new_received"
-NavigationBar_New_Received_Files_Number = "id=com.filestring.lattedouble:id/drawer_new_received_num"
-NavigationBar_New_Received_Updates = "id=com.filestring.lattedouble:id/drawer_updated_received"
-NavigationBar_New_Received_Updates_Number = "id=com.filestring.lattedouble:id/drawer_updated_received_num"
-NavigationBar_Update_To_Push = "id=com.filestring.lattedouble:id/drawer_ready_push_update"
-NavigationBar_Update_To_Push_Number = "id=com.filestring.lattedouble:id/drawer_ready_push_update_num"
+NavigationBar_EmailAddress = "id=com.filestring.lattedouble:id/drawer_email"
+NavigationBar_NotificationBell = "id=com.filestring.lattedouble:id/drawer_notifications"
+NavigationBar_UserExpand = "id=com.filestring.lattedouble:id/drawer_users_expand"
+NavigationBar_AllFiles = "id=com.filestring.lattedouble:id/drawer_my_files"
+NavigationBar_NewReceivedFiles = "id=com.filestring.lattedouble:id/drawer_new_received"
+NavigationBar_NewReceivedFilesNumber = "id=com.filestring.lattedouble:id/drawer_new_received_num"
+NavigationBar_NewReceivedUpdates = "id=com.filestring.lattedouble:id/drawer_updated_received"
+NavigationBar_NewReceivedUpdatesNumber = "id=com.filestring.lattedouble:id/drawer_updated_received_num"
+NavigationBar_UpdateToPush = "id=com.filestring.lattedouble:id/drawer_ready_push_update"
+NavigationBar_UpdateToPushNumber = "id=com.filestring.lattedouble:id/drawer_ready_push_update_num"
 NavigationBar_Downloads = "id=com.filestring.lattedouble:id/drawer_download"
-NavigationBar_Downloads_Number = "id=com.filestring.lattedouble:id/drawer_download_num"
+NavigationBar_DownloadsNumber = "id=com.filestring.lattedouble:id/drawer_download_num"
 NavigationBar_People = "id=com.filestring.lattedouble:id/drawer_category_people"
 NavigationBar_Settings = "id=com.filestring.lattedouble:id/drawer_settings"
-NavigationBar_Help_Feedback = "id=com.filestring.lattedouble:id/drawer_help"
+NavigationBar_HelpFeedback = "id=com.filestring.lattedouble:id/drawer_help"
 
 
 """
@@ -115,35 +115,147 @@ NavigationBar_Help_Feedback = "id=com.filestring.lattedouble:id/drawer_help"
 UserInfo_TitleID = "id=android:id/action_bar_title"
 UserInfo_TitleText = "User Info"
 UserInfo_Email = "id=com.filestring.lattedouble:id/txtmy_account_email_value"
-UserInfo_First_Name = "id=com.filestring.lattedouble:id/txtmy_account_first_name_value"
-UserInfo_Last_Name = "id=com.filestring.lattedouble:id/txtmy_account_last_name_value"
-UserInfo_Change_Pasword = "id=com.filestring.lattedouble:id/txtmy_account_change_password"
-UserInfo_Sign_Out = "id=com.filestring.lattedouble:id/txtmy_account_log_out"
+UserInfo_FirstName = "id=com.filestring.lattedouble:id/txtmy_account_first_name_value"
+UserInfo_LastName = "id=com.filestring.lattedouble:id/txtmy_account_last_name_value"
+UserInfo_ChangePasword = "id=com.filestring.lattedouble:id/txtmy_account_change_password"
+UserInfo_SignOut = "id=com.filestring.lattedouble:id/txtmy_account_log_out"
 
 """
 @Author: Linh.nguyen
 @Date Created: Jan 22, 2015
 @Description: All elements in Change Password Screen
 """
-ChangePassword_Current_Password = "id=com.filestring.lattedouble:id/edt_current_pass"
-ChangePassword_New_Password = "id=com.filestring.lattedouble:id/edt_change_pass_1"
-ChangePassword_Confirm_Password = "id=com.filestring.lattedouble:id/edt_change_pass_2"
-ChangePassword_Show_Hide_Password = "id=com.filestring.lattedouble:id/chb_ShowPass"
-ChangePassword_Save_Button = "id=com.filestring.lattedouble:id/txtChange"
-
-
-
-
+ChangePassword_CurrentPassword = "id=com.filestring.lattedouble:id/edt_current_pass"
+ChangePassword_NewPassword = "id=com.filestring.lattedouble:id/edt_change_pass_1"
+ChangePassword_ConfirmPassword = "id=com.filestring.lattedouble:id/edt_change_pass_2"
+ChangePassword_ShowHidePassword = "id=com.filestring.lattedouble:id/chb_ShowPass"
+ChangePassword_SaveButton = "id=com.filestring.lattedouble:id/txtChange"
 
 """
 @Author: Linh.nguyen
 @Date Created: Jan 22, 2015
 @Description: All elements in Reset Password Screen
 """
-ResetPassword_Your_Email = "id=com.filestring.lattedouble:id/edt_email"
-ResetPassword_Send_Button  = "id=com.filestring.lattedouble:id/txt_send"
+ResetPassword_EmailAddress = "id=com.filestring.lattedouble:id/edt_email"
+ResetPassword_SendButton  = "id=com.filestring.lattedouble:id/txt_send"
 
 
+"""
+@Author: Minh.nguyen
+@Date Created: Feb 26, 2015
+@Description: All elements in Dropbox Application
+"""
+Dropbox_Tour_SignIn = "id=com.dropbox.android:id/tour_sign_in"
+Dropbox_SignUp_SignIn = "id=com.dropbox.android:id/login_screen_switch_to_login"
+
+Dropbox_SignIn_Email = "id=com.dropbox.android:id/login_email"
+Dropbox_SignIn_Password = "id=com.dropbox.android:id/login_password"
+Dropbox_SignIn_SignInButton = "id=com.dropbox.android:id/login_submit"
+
+Dropbox_ChooseFile_Title = "Choose file"
+Dropbox_ChooseFile_FileName = "id=com.dropbox.android:id/filelist_name"
+Dropbox_Home_MoreOption = "context-desc=More options"
+Dropbox_Home_MoreOption_Settings = "text=Settings"
+Dropbox_Settings_SignOut = "text=Sign out of Dropbox"
+
+
+
+
+"""
+@Author: Minh.nguyen
+@Date Created: Feb 27, 2015
+@Description: All elements in Dropbox Application
+"""
+StringAFileFrom_Title = "id=com.filestring.lattedouble:id/dlg_title"
+StringAFileFrom_Documents = "//android.widget.ListView/android.widget.LinearLayout[1]"
+
+
+
+
+
+
+"""
+@Author: Minh.nguyen
+@Date Created: March 05, 2015
+@Description: All elements in String To Recipient
+"""
+StringToRecipient_SendButton = "id=com.filestring.lattedouble:id/menu_recipients_send"
+StringToRecipient_FileName = "id=com.filestring.lattedouble:id/txt_add_receipient_file_name"
+StringToRecipient_Recipient = "id=com.filestring.lattedouble:id/auto_text_add_recipient"
+StringToRecipient_AddEmail = "id=com.filestring.lattedouble:id/recipients_add_email"
+StringToRecipient_AccessLevel = "id=com.filestring.lattedouble:id/access_level_value"
+StringToRecipient_Expiration = "id=com.filestring.lattedouble:id/expiration_value"
+StringToRecipient_Watermark = "id=com.filestring.lattedouble:id/sw_watermark"
+StringToRecipient_PersonalNote = "id=com.filestring.lattedouble:id/edt_person_note"
+
+
+
+
+
+
+"""
+@Author: Minh.nguyen
+@Date Created: March 05, 2015
+@Description: All elements in Google Drive Screen
+"""
+GoogleDrive_SelectGoogleAccount_AddAccountButton = "//android.widget.CheckedTextView[@text='Add account']"
+GoogleDrive_SelectGoogleAccount_OKButton = "id=android:id/button2"
+GoogleDrive_AddAGoogleAccount_ExistingButton = "id=com.google.android.gsf.login:id/next_button"
+GoogleDrive_SignIn_EmailAddress = "id=com.google.android.gsf.login:id/username_edit"
+GoogleDrive_SignIn_Password = "id=com.google.android.gsf.login:id/password_edit"
+GoogleDrive_SignIn_BackButton = "id=com.google.android.gsf.login:id/back_button"
+GoogleDrive_SignIn_NextButton = "id=com.google.android.gsf.login:id/next_button"
+GoogleDrive_SignIn_OKButton = "id=android:id/button1"
+GoogleDrive_UpgradeToGooglePlus_NotNow = "id=com.google.android.gsf.login:id/skip_button"
+GoogleDrive_Services_checkbox = "id=com.google.android.gsf.login:id/google_play_opt_in"
+
+
+
+
+
+
+
+"""
+@Author: Minh.nguyen
+@Date Created: March 05, 2015
+@Description: All elements in Google Drive Screen
+"""
+
+
+
+
+
+
+"""
+@Author: Minh.nguyen
+@Date Created: March 10, 2015
+@Description: All elements in List of Files
+"""
+ListFiles_FileName = "id=com.filestring.lattedouble:id/txtfile_item_name"
+ListFiles_About = "id=com.filestring.lattedouble:id/about"
+
+
+"""
+@Author: Minh.nguyen
+@Date Created: March 10, 2015
+@Description: All elements in File Details Screen
+"""
+FileDetails_MenuDetails = "id=com.filestring.lattedouble:id/action_menu_details"
+FileDetails_MenuDetails_ViewFile = "id=com.filestring.lattedouble:id/menu_view_file"
+FileDetails_MenuDetails_Rename = "id=com.filestring.lattedouble:id/menu_rename"
+FileDetails_MenuDetails_MoveTo = "id=com.filestring.lattedouble:id/menu_move_to_folder"
+FileDetails_MenuDetails_String = "id=com.filestring.lattedouble:id/menu_add_recipient"
+FileDetails_MenuDetails_ModifyRecipients = "id=com.filestring.lattedouble:id/menu_modify_recipients"
+FileDetails_MenuDetails_Delete = "id=com.filestring.lattedouble:id/menu_delete"
+FileDetails_Rename = "id=com.filestring.lattedouble:id/detail_imgbt_rename"
+FileDetails_Rename_PopupTitle = "id=com.filestring.lattedouble:id/dlg_title"
+FileDetails_Rename_PopupTextField = "//android.widget.EditText"
+FileDetails_Rename_PopupCancelButton = "id=com.filestring.lattedouble:id/dlg_btn_negative"
+FileDetails_Rename_PopupOKButton = "id=com.filestring.lattedouble:id/dlg_btn_positive"
+FileDetails_AvaiableOffline = "id=com.filestring.lattedouble:id/offline_switch"
+FileDetails_ShowRevokedRecipients = "id=com.filestring.lattedouble:id/show_revoked_switch"
+FileDetails_FileInformation = "id=com.filestring.lattedouble:id/txt_file_info"
+FileDetails_AddRecipient = "id=com.filestring.lattedouble:id/img_add_people"
 
 
 
